e2050eafc4314153282543e87c65588a1d227a95
last-saint <uppalapraveen1990@hotmail.com>
2025-06-07 13:27:59 +0530

    created accounts micro service in springboot

---
+++ src/test/java/com/up7/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.up7.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
